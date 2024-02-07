1c7905a8dc9df1c31d225a5add1019de4ccfe019
Cass <cassianoleaoramos@gmail.com>
2024-02-06 22:07:11 -0300

    Initial commit

---
+++ src/test/java/com/cass/process_backend/ProcessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cass.process_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
