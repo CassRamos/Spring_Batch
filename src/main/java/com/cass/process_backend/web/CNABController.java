b0634a17359e24e5b3c7d770b7ff7db576a45bf9
Cass <cassianoleaoramos@gmail.com>
2024-02-11 17:25:42 -0300

    Implement upload files on `tmp` directory

--- src/main/java/com/cass/process_backend/web/CNABController.java
+++ src/main/java/com/cass/process_backend/web/CNABController.java
@@ -1,5 +1,6 @@
 package com.cass.process_backend.web;
 
+import com.cass.process_backend.domain.CNABService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +10,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("cnab")
 public class CNABController {
+
+    private final CNABService cnabService;
+
+    public CNABController(CNABService cnabService) {
+        this.cnabService = cnabService;
+    }
+
     @PostMapping("upload")
-    public String upload(@RequestParam("file") MultipartFile file) {
+    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
+        cnabService.uploadCnabFile(file);
         return "Process initialized!";
     }
 }
