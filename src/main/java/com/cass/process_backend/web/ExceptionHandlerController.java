bf3c24c10629ff89a2c584e3bd24a8adfe40ddb3
Cass <cassianoleaoramos@gmail.com>
2024-02-11 19:11:14 -0300

    Create exception handler class

---
+++ src/main/java/com/cass/process_backend/web/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package com.cass.process_backend.web;
+
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+
+    @ExceptionHandler(JobInstanceAlreadyCompleteException.class)
+    private ResponseEntity<Object> handleFileAlreadyImported(JobInstanceAlreadyCompleteException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                "This file is already imported!"
+        );
+    }
+}
